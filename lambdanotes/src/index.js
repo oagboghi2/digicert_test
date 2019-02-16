72491239e84320e8a3cb8b095341bdd49d8bf1da
Obo Agboghidi <oagboghi2@gmail.com>
2019-02-15 18:28:03 -0800

    set up dependencies, created dummRedux

--- lambdanotes/src/index.js
+++ lambdanotes/src/index.js
@@ -2,9 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter as Router} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const dummyReducer = (state = [], action) => state; //extract into it's own file.
+//first argument is the state, 2nd is the action that returns a modified state/
+
+const store = createStore(dummyReducer);
+
+ReactDOM.render(
+    (
+    <Provider store={store} /*Provider takes store as a prop to connect redux to react*/>
+        <Router>
+            <App />
+        </Router>
+    </Provider>
+    )
+    , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
