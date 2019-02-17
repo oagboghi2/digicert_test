169afe70ddc3c9443434444f3e93bc67dd6282b7
Obo Agboghidi <oagboghi2@gmail.com>
2019-02-16 17:02:39 -0800

    Styled notelist component, hardcoded note dummy data

--- lambdanotes/src/components/NotesList/NotesList.js
+++ lambdanotes/src/components/NotesList/NotesList.js
@@ -4,7 +4,55 @@ import './index.css';
 class NotesList extends Component {
     state = {  }
     render() { 
-        return ( <div>Notes List Placeholder</div> );
+        return ( 
+        <div className = 'noteslist_container'>
+        <div><h3 className = 'contentHeader'>Your Notes:</h3></div>
+        <div className = 'notesList'>
+                <div className = 'note'>
+                    <div>
+                        <h4>Note Title</h4>
+                        <hr></hr>
+                        <p>Lorem ipsum </p>
+                    </div>
+                </div>
+                <div className = 'note'>
+                    <div>
+                        <h4>Note Title</h4>
+                        <hr></hr>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sit amet tortor eu ex varius bibendum a malesuada risus. Sed facilisis orci ac bibendum imperdiet. Integer non lectus tincidunt eros elementum consectetur vel quis nibh. Cras a dapibus lectus. Integer id pulvinar quam, vitae sollicitudin lorem. Nulla et elementum risus. Aliquam molestie, justo a sodales dictum, ligula ante fermentum quam, id vulputate ligula leo id mi. Mauris pretium metus in nunc ornare, vitae rutrum massa tincidunt.</p>
+                    </div>
+                </div>
+                <div className = 'note'>
+                    <div>
+                        <h4>Note Title</h4>
+                        <hr></hr>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sit amet tortor eu ex varius bibendum a malesuada risus. Sed facilisis orci ac bibendum imperdiet. Integer non lectus tincidunt eros elementum consectetur vel quis nibh. Cras a dapibus lectus. Integer id pulvinar quam, vitae sollicitudin lorem. Nulla et elementum risus. Aliquam molestie, justo a sodales dictum, ligula ante fermentum quam, id vulputate ligula leo id mi. Mauris pretium metus in nunc ornare, vitae rutrum massa tincidunt.</p>
+                    </div>
+                </div>
+                <div className = 'note'>
+                    <div>
+                        <h4>Note Title</h4>
+                        <hr></hr>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sit amet tortor eu ex varius bibendum a malesuada risus. Sed facilisis orci ac bibendum imperdiet. Integer non lectus tincidunt eros elementum consectetur vel quis nibh. Cras a dapibus lectus. Integer id pulvinar quam, vitae sollicitudin lorem. Nulla et elementum risus. Aliquam molestie, justo a sodales dictum, ligula ante fermentum quam, id vulputate ligula leo id mi. Mauris pretium metus in nunc ornare, vitae rutrum massa tincidunt.</p>
+                    </div>
+                </div>
+                <div className = 'note'>
+                    <div>
+                        <h4>Note Title</h4>
+                        <hr></hr>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sit amet tortor eu ex varius bibendum a malesuada risus. Sed facilisis orci ac bibendum imperdiet. Integer non lectus tincidunt eros elementum consectetur vel quis nibh. Cras a dapibus lectus. Integer id pulvinar quam, vitae sollicitudin lorem. Nulla et elementum risus. Aliquam molestie, justo a sodales dictum, ligula ante fermentum quam, id vulputate ligula leo id mi. Mauris pretium metus in nunc ornare, vitae rutrum massa tincidunt.</p>
+                    </div>
+                </div>
+                <div className = 'note'>
+                    <div>
+                        <h4>Note Title</h4>
+                        <hr></hr>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sit amet tortor eu ex varius bibendum a malesuada risus. Sed facilisis orci ac bibendum imperdiet. Integer non lectus tincidunt eros elementum consectetur vel quis nibh. Cras a dapibus lectus. Integer id pulvinar quam, vitae sollicitudin lorem. Nulla et elementum risus. Aliquam molestie, justo a sodales dictum, ligula ante fermentum quam, id vulputate ligula leo id mi. Mauris pretium metus in nunc ornare, vitae rutrum massa tincidunt.</p>
+                    </div>
+                </div>
+            </div>
+        </div> 
+        );
     }
 }
  
--- lambdanotes/src/components/mainContent/MainContent.js
+++ lambdanotes/src/components/mainContent/MainContent.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import './index.css';
+import NotesList from '../NotesList/NotesList.js'
 
 class MainContent extends Component {
     state = {  }
     render() { 
         return ( 
         <div className = 'main_container'>
-            Main Content Placeholder
+            <NotesList/>
         </div> );
     }
 }
