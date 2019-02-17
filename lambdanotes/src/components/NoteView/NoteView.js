0945a7e8660e940c4a95e33c4853cdd9a7478227
Obo Agboghidi <oagboghi2@gmail.com>
2019-02-17 02:25:31 -0800

    Styled noteView component

--- lambdanotes/src/components/NoteView/NoteView.js
+++ lambdanotes/src/components/NoteView/NoteView.js
@@ -2,10 +2,56 @@ import React, { Component } from 'react'
 import './index.css';
 
 class NoteView extends Component {
-    state = {  }
-    render() { 
-        return ( <div>Note View Placeholder</div> );
+    state = { 
+        notesArray: [
+            {
+                _id: 'fhcdjnmk',
+                title: 'First Note',
+                body: 'Integer sit amet tortor eu ex varius bibendum a malesuada risus. Sed facilisis orci ac bibendum imperdiet. Integer non lectus tincidunt eros elementum consectetur vel quis nibh. Cras a dapibus lectus. Integer id pulvinar quam, vitae sollicitudin lorem. Nulla et elementum risus. Aliquam molestie, justo a sodales dictum, ligula ante fermentum quam, id vulputate ligula leo id mi. Mauris pretium metus in nunc ornare, vitae rutrum massa tincidunt.Cras mollis commodo sapien, id gravida neque condimentum dictum.Proin non magna ultricies, rhoncus elit molestie, maximus tellus.Proin ac rutrum ligula.Etiam consectetur mi sed ante finibus, eu consequat purus imperdiet.Maecenas imperdiet interdum ante vitae sodales.Praesent id tristique mauris.Aenean mollis porttitor faucibus.Morbi vitae scelerisque ex.Aenean eget placerat augue.Nam facilisis vel magna quis condimentum.Maecenas faucibus pulvinar magna, ut euismod lacus suscipit sit amet.',
+                CreatedAt: 1550384469279
+            },
+            {
+                _id: 'fvejnkmjnmvfem',
+                title: 'Second Note',
+                body: 'Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet,',
+                CreatedAt: 1550384469279
+            },
+            {
+                _id: 'fiyuouiyu',
+                title: 'Third Note',
+                body: ' consectetur adipiscing',
+                CreatedAt: 1550384469279
+            },
+            {
+                _id: 'jbknlkyu',
+                title: 'Fourth Note',
+                body: ' consectetur adipiscing',
+                CreatedAt: 1550384469279
+            },
+        ]
     }
-}
- 
+
+    
+
+    render() { 
+        return ( 
+            <div className = 'noteview_container'>
+            <div className = 'noteview_Topcontent'>
+                    <h3 className = 'contentHeader'>
+                        {this.state.notesArray[0].title}
+                    </h3>
+                    <div>
+                        <a className = 'edit_delete'> Edit</a>
+                        <a className = 'edit_delete'> Delete</a>
+                    </div>
+            </div>
+                <hr></hr>
+                <div className='notesList'>
+                    <p className = ''>
+                        {this.state.notesArray[0].body}
+                    </p>
+                </div>
+            </div>
+        )}
+    }
 export default NoteView;
--- lambdanotes/src/components/mainContent/MainContent.js
+++ lambdanotes/src/components/mainContent/MainContent.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react'
 import './index.css';
 import NotesList from '../NotesList/NotesList.js'
+import NoteView from '../NoteView/NoteView.js'
 
 class MainContent extends Component {
     state = {  }
     render() { 
         return ( 
         <div className = 'main_container'>
-            <NotesList/>
+            <NoteView/>
         </div> );
     }
 }
